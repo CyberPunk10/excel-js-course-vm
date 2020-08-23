10518d8194fde1c48046303220873a7816895987
CyberPunk10 <konstantin21_arx@gmail.com>
2020-08-23 20:00:16 +0300

    Development

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,8 @@
 import { ExcelComponent } from '../../core/ExcelComponent'
 
 export class Table extends ExcelComponent {
+  static className = 'excel__table'
+
   toHTML() {
     return '<h1>Table</h1>'
   }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,6 +1,8 @@
 import { ExcelComponent } from '../../core/ExcelComponent';
 
 export class Toolbar extends ExcelComponent {
+  static className = 'excel__toolbar'
+
   toHTML() {
     return '<h1>Toolbar</h1>'
   }
