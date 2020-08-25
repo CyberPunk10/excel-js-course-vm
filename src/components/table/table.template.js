79c6d2dd9238c9c75c234dcd009ab09e96eb4d5b
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2020-08-25 21:44:39 +0300

    Created Table

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,3 +1,44 @@
-export function createTable() {
-  return '<h1>test</h1>'
+export function createTable(rowsCount = 21) {
+  let html = ''
+  for (let i = 0; i <= rowsCount; i++) {
+    html += createRow(i) // numberRow
+  }
+  return html
+}
+
+const CODES = {
+  'A': 65,
+  'Z': 90
+}
+
+function toChar(charCode) {
+  return String.fromCharCode(charCode)
+}
+
+function createRow(numberRow) {
+  const colsCount = CODES.Z - CODES.A
+  let fullCol = ''
+
+  for (let i = 0; i <= colsCount; i++) {
+    fullCol += (numberRow === 0) ? createCol(CODES.A + i) : createCol()
+  }
+
+  const html = `
+    <div class="excel-table__row">
+      <div class="excel-table__row-info">${numberRow ? numberRow : ''}</div>
+      <div class="excel-table__row-data">${fullCol}</div>
+    </div>
+  `
+  return html
+}
+
+function createCol(charCode) {
+  if (charCode) {
+    return `
+      <div class="excel-table__column">${toChar(charCode)}</div>
+    `
+  }
+  return `
+    <div class="excel-table__cell" contenteditable></div>
+  `
 }
