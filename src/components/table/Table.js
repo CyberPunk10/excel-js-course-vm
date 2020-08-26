2ed228dd48f666f4b36d3cec7052b446861de252
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2020-08-27 01:19:11 +0300

    Development resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -27,23 +27,19 @@ export class Table extends ExcelComponent {
       const $target = event.target
       // const $parent = $target.parentElement
       const $parent = $target.closest('[data-type="resizable"]')
-      const cTt = $target.getBoundingClientRect()
       const coords = $parent.getBoundingClientRect()
-      // const currentPageX = event.pageX;
-      // (2) отследить позиционирование на элементе
-      const offsetX = event.offsetX;
-      console.log(offsetX)
-      console.log('parent: ', coords)
+      const coordsTt = $target.getBoundingClientRect()
+      const offsetX = event.offsetX // отследить позиционирование на элементе
+
       document.onmousemove = e => {
         if (event.target.dataset.resize === 'col') {
           // из расстояния от левого края страницы до курсора отнимаем
           // расстояние от левшго края страницы до крайней-правой координаты колонки
           // и прибавляем разницу между шириной блока и координаты курсора от левого края блока
           // и прибавляем +1 (ширина бордера колонки)
-          const delta = e.pageX - coords.right + cTt.width - offsetX + 1
+          const delta = e.pageX - coords.right + coordsTt.width - offsetX + 1
           const value = coords.width + delta
           $parent.style.width = value + 'px'
-          console.log(e.pageX)
         } else if (event.target.dataset.resize === 'row') {
           const delta = e.pageY - coords.bottom
           const value = coords.height + delta
