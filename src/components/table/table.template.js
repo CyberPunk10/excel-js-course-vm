9b0cb06a89d33a721bcb81645903fc771f9f869a
CyberPunk_xiaomi <konstantin21_arx@bk.ru>
2020-09-06 00:24:58 +0300

    Development state

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -19,6 +19,7 @@ function toChar(charCode) {
   return String.fromCharCode(charCode)
 }
 
+// Create Row
 function createRow(numberRow, state) {
   const colsCount = CODES.Z - CODES.A
   let fullCol = ''
@@ -31,7 +32,7 @@ function createRow(numberRow, state) {
   const height = rowState[numberRow] ? `style="height: ${rowState[numberRow]}px"` : ''
 
   const html = `
-    <div class="excel-table__row" ${numberRow ? 'data-type="resizable"' : ''} data-number-row="${numberRow}" id="row-${numberRow}" ${height}>
+    <div class="excel-table__row" ${numberRow ? 'data-type="resizable"' : ''} data-number-row="${numberRow}" ${height}>
       <div class="excel-table__row-info">
         ${numberRow ? numberRow : ''}
         ${numberRow ? '<div class="row-resize" data-resize="row"></div>' : ''}
@@ -42,21 +43,34 @@ function createRow(numberRow, state) {
   return html
 }
 
+// Create Col
 function createCol(numberRow, charCode, state) {
   const numberCol = charCode - CODES.varForIndex
 
   const colState = state.colState
   const width = colState[numberCol] ? `style="width: ${colState[numberCol]}px"` : ''
-  console.log(state)
+  const idCell = `${numberRow}:${numberCol}`
+
   if (!numberRow) { // если это 0-вая строка, то записывем в неё charCode
     return `
-      <div class="excel-table__column" data-type="resizable" data-number-col="${numberCol}" ${width}>
-        ${toChar(charCode)}
-        <div class="col-resize" data-resize="col"></div>
+      <div 
+        class="excel-table__column" 
+        data-type="resizable"
+        data-number-col="${numberCol}"
+        ${width}
+        >
+          ${toChar(charCode)}
+          <div class="col-resize" data-resize="col"></div>
       </div>
     `
   }
   return `
-  <div class="excel-table__cell" contenteditable data-number-col="${numberCol}" data-row-col="${numberRow}:${numberCol}" ${width}>${state.dataState.id}</div>
+  <div 
+    class="excel-table__cell"
+    contenteditable
+    data-number-col="${numberCol}"
+    data-row-col="${idCell}"
+    ${width}
+    >${state.dataState[`${idCell}`] || ''}</div>
 `
 }
