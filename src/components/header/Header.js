0e7b27045d3e3a79032acc9d8cbff20caef50603
CyberPunk10 <konstantin21_arx@gmail.com>
2020-09-12 16:59:57 +0300

    Finished lesson 06_21 - naiming Table

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,6 @@
 import { ExcelComponent } from '../../core/ExcelComponent'
+import { defaultTitle } from '../../constans'
+import { changeTitle } from '../../redux/actions'
 
 export class Header extends ExcelComponent {
   constructor($root, options) {
@@ -12,8 +14,9 @@ export class Header extends ExcelComponent {
   static className = 'excel-header'
 
   toHTML() {
+    const title = this.store.getState().titleTable || defaultTitle
     return `
-      <input class="excel-header__input" type="text" value="Новая таблица" />
+      <input class="excel-header__input" type="text" value="${title}" />
       <div class="excel-header__buttons">
         <div class="excel-header__button"><span class="material-icons">delete</span></div>
         <div class="excel-header__button"><span class="material-icons">exit_to_app</span></div>
@@ -24,12 +27,11 @@ export class Header extends ExcelComponent {
   onInput(event) {
     console.log('[Header] Listener: input', event.target.value.trim())
     // console.log(event.target.textContent)
-    console.log(this.$root)
+    this.$dispatch(changeTitle(event.target.value.trim()))
   }
 
   onClick(event) {
     console.log('[Header] Listener: click', event.target)
     // console.log(event.target.textContent)
-    console.log(this.$root)
   }
 }
