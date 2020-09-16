8ca84da13a22db52d13b0369709f7de84101580f
CyberPunk10 <konstantin21_arx@gmail.com>
2020-09-16 18:37:32 +0300

    Development Routing

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -3,8 +3,7 @@ import { Emitter } from '../../core/Emitter'
 import { StoreSubscriber } from '../../core/storeSubscriber'
 
 export class Excel {
-  constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+  constructor(options) {
     this.components = options.components || []
     this.store = options.store
     this.emitter = new Emitter()
@@ -33,12 +32,10 @@ export class Excel {
       $root.append($el)
       return component
     })
-
     return $root
   }
 
-  render() {
-    this.$el.append(this.getRoot())
+  init() {
     this.subscriber.subscribeComponents(this.components)
     this.components.forEach( component => component.init() )
   }
